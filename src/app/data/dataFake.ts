0b2d028d3a04aaf451c6da6638d32c86b278ea66
Caio Franco <caiofranco.assaoka@gmail.com>
2023-10-18 11:35:09 -0300

    content createad

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVA TEMPORADA DE BETTER CALL SAUL ANUNCIADA",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer faucibus mauris id diam placerat, eu sagittis augue pellentesque. Ut vestibulum, quam sit amet interdum tincidunt, leo justo volutpat mauris, at consectetur risus ante non tortor. Nunc tempus semper viverra. Sed vel vehicula tellus. Nam consectetur, risus nec bibendum finibus, ligula augue dapibus nulla,",
+    "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2022/06/better-call-saul-cardiaco.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Breaking Bad anuncia seu retorno!",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer faucibus mauris id diam placerat, eu sagittis augue pellentesque. Ut vestibulum, quam sit amet interdum tincidunt, leo justo volutpat mauris, at consectetur risus ante non tortor. Nunc tempus semper viverra. Sed vel vehicula tellus. Nam consectetur, risus nec bibendum finibus, ligula augue dapibus nulla",
+    "photoCover":"https://akamai.sscdn.co/uploadfile/letras/playlists/7/6/6/0/766046eb2cc94d19b9fc29123e453868.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Nova temporada de Loki já disponível no Disney +",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer faucibus mauris id diam placerat, eu sagittis augue pellentesque. Ut vestibulum, quam sit amet interdum tincidunt, leo justo volutpat mauris, at consectetur risus ante non tortor. Nunc tempus semper viverra. Sed vel vehicula tellus. Nam consectetur, risus nec bibendum finibus, ligula augue dapibus nulla",
+    "photoCover":"https://t.ctcdn.com.br/txF-4Wlx4c1V0wSFU5AVc91uLho=/0x272:2157x1487/2157x1213/smart/i474136.jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Os principais personagens da saga Star Wars",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer faucibus mauris id diam placerat, eu sagittis augue pellentesque. Ut vestibulum, quam sit amet interdum tincidunt, leo justo volutpat mauris, at consectetur risus ante non tortor. Nunc tempus semper viverra. Sed vel vehicula tellus. Nam consectetur, risus nec bibendum finibus, ligula augue dapibus nulla",
+    "photoCover":"https://proxy.olhardigital.com.br/wp-content/uploads/2023/03/Star-Wars.jpg"
+  }
+]
